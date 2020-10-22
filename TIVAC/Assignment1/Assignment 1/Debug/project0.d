ad2e4625e2ec7de8b363f2cc1240a35a379a2eec
echevary <60421885+echevary@users.noreply.github.com>
2020-10-21 23:44:20 -0700

    Add files via upload

---
+++ TIVAC/Assignment1/Assignment 1/Debug/project0.d
@@ -0,0 +1,45 @@
+# FIXED
+
+project0.obj: ../project0.c
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/stdint.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/_stdint40.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/sys/stdint.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/sys/cdefs.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/sys/_types.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/machine/_types.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/machine/_stdint.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/sys/_stdint.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/stdbool.h
+project0.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h
+project0.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h
+project0.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h
+project0.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h
+project0.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h
+project0.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h
+project0.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_ints.h
+project0.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/interrupt.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/string.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/_ti_config.h
+project0.obj: C:/ti/ti-cgt-arm_18.12.5.LTS/include/linkage.h
+
+../project0.c: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/stdint.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/_stdint40.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/sys/stdint.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/sys/cdefs.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/sys/_types.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/machine/_types.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/machine/_stdint.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/sys/_stdint.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/stdbool.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_ints.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/interrupt.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/string.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/_ti_config.h: 
+C:/ti/ti-cgt-arm_18.12.5.LTS/include/linkage.h: 
